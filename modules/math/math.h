d85ad71be1d05bf5c40316a6b64226e5f854d66d
DrJegesmedve <105545960+DrJegesmedve@users.noreply.github.com>
2025-03-07 13:32:07 +0100

    Update math.h

--- modules/math/math.h
+++ modules/math/math.h
@@ -5,5 +5,6 @@
 class Module {
 public:
     virtual void execute(const std::vector<std::string>& args) = 0;
+    virtual std::string getVersion() const { return "Math Module Version 1.1.0"; }
     virtual ~Module() {}
 };
